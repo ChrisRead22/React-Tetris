5b8e26ecdda28fe1ebc21fcc21a91b23226d000b
Christopher Read <Christopherread22@outlook.com>
2022-08-05 18:53:21 -0400

    Tetro

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -2,7 +2,10 @@ export const TETROMINOS = {
     0: { shape: [[0]], color: '0, 0, 0' }, 
     I: {
         shape: [
-            [0]
+            [0, 'I', 0, 0],
+            [0, 'I', 0, 0],
+            [0, 'I', 0, 0],
+            [0, 'I', 0, 0]
         ]
     }
 
