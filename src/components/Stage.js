bb870e4296a961b26c587122266fe85af7d6b376
Christopher Read <Christopherread22@outlook.com>
2022-08-05 18:28:37 -0400

    Stage

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -3,7 +3,7 @@ import Cell from './Cell';
 
 const Stage = ({ stage }) => (
     <div>
-        {stage.map(row => row.map((cell, x) => ))}
+        {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
         
     </div>
 );
